ab5a30e6d4ffa17a2644b2e64807a2760b9d7ef1
Sangita <mistymoni5050@gmail.com>
2024-09-19 21:55:16 +0600

    added all

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 
 
 const ServiceCard = ({service}) => {
-    const {title, img, price} = service;
+    const {_id, title, img, price} = service;
 
     return (
         <div className="card bg-base-100 w-96 shadow-xl">
@@ -15,7 +16,9 @@ const ServiceCard = ({service}) => {
     <h2 className="card-title">{title}</h2>
     <p className="text-xl text-orange-500">Price: ${price}</p>
     <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
+    <Link to={`/book/${_id}`}>
+        <button className="btn btn-primary">Book Now</button>
+    </Link>
     </div>
   </div>
 </div>
